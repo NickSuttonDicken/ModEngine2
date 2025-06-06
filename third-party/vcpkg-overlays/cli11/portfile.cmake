0fb7e27b9aaaecf46db49c9ae64d57bf27c901c0
Nick Sutton-Dicken <nicholassuttondicken@gmail.com>
2025-06-06 23:27:08 +1200

    another attempt

--- third-party/vcpkg-overlays/cli11/portfile.cmake
+++ third-party/vcpkg-overlays/cli11/portfile.cmake
@@ -1,5 +1,3 @@
-# vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CLIUtils/CLI11
@@ -8,19 +6,22 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
+# 🩹 Patch CMakeLists.txt to support CMake 3.5+
+file(READ "${SOURCE_PATH}/CMakeLists.txt" _contents)
+string(REPLACE "cmake_minimum_required(VERSION 2.8)" "cmake_minimum_required(VERSION 3.5)" _contents "${_contents}")
+file(WRITE "${SOURCE_PATH}/CMakeLists.txt" "${_contents}")
+
 vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
         -DCLI11_BUILD_EXAMPLES=OFF
         -DCLI11_BUILD_DOCS=OFF
         -DCLI11_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/CLI11)
 vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-# Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
